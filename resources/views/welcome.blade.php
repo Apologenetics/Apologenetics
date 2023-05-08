164070cb427f0faf7fff824b38a7452c81afbba6
Dominic Sears <dominicsears@hotmail.com>
2023-05-08 08:18:19 -0700

    Updated links to open in new tab

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,20 +63,20 @@
                     <!-- Social -->
                     <div class="flex flex-row space-x-4 w-full justify-center items-center">
                         <!-- Twitter -->
-                        <a href="https://twitter.com/dominicsears">
+                        <a target="_blank" href="https://twitter.com/dominicsears">
                             <svg version="1.1" class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 248 204" style="enable-background:new 0 0 248 204;" xml:space="preserve">
                                 <path d="M221.95,51.29c0.15,2.17,0.15,4.34,0.15,6.53c0,66.73-50.8,143.69-143.69,143.69v-0.04   C50.97,201.51,24.1,193.65,1,178.83c3.99,0.48,8,0.72,12.02,0.73c22.74,0.02,44.83-7.61,62.72-21.66   c-21.61-0.41-40.56-14.5-47.18-35.07c7.57,1.46,15.37,1.16,22.8-0.87C27.8,117.2,10.85,96.5,10.85,72.46c0-0.22,0-0.43,0-0.64   c7.02,3.91,14.88,6.08,22.92,6.32C11.58,63.31,4.74,33.79,18.14,10.71c25.64,31.55,63.47,50.73,104.08,52.76   c-4.07-17.54,1.49-35.92,14.61-48.25c20.34-19.12,52.33-18.14,71.45,2.19c11.31-2.23,22.15-6.38,32.07-12.26   c-3.77,11.69-11.66,21.62-22.2,27.93c10.01-1.18,19.79-3.86,29-7.95C240.37,35.29,231.83,44.14,221.95,51.29z"/>
                             </svg>
                         </a>
                         <!-- YouTube -->
-                        <a href="https://www.youtube.com/channel/UCWv_Ig9CRrwrjEtr2P0TM3Q">
+                        <a target="_blank" href="https://www.youtube.com/channel/UCWv_Ig9CRrwrjEtr2P0TM3Q">
                             <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 200 100">
                                 <path d="m154 17.5c-1.82-6.73-7.07-12-13.8-13.8-9.04-3.49-96.6-5.2-122 0.1-6.73 1.82-12 7.07-13.8 13.8-4.08 17.9-4.39 56.6 0.1 74.9 1.82 6.73 7.07 12 13.8 13.8 17.9 4.12 103 4.7 122 0 6.73-1.82 12-7.07 13.8-13.8 4.35-19.5 4.66-55.8-0.1-75z" fill="currentColor"/>
                                 <path d="m105 55-40.8-23.4v46.8z" fill="#fff"/>
                             </svg>
                         </a>
                         <!-- Instagram -->
-                        <a href="https://www.instagram.com/dominicsears/">
+                        <a target="_blank" href="https://www.instagram.com/dominicsears/">
                             <svg version="1.1" class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 500 500" style="enable-background:new 0 0 500 500;" xml:space="preserve">
                                 <g>
                                     <g>
@@ -115,7 +115,7 @@
                     </div>
                     <!-- Links -->
                     <div class="flex flex-row space-x-4 flex-wrap w-full justify-center">
-                        <a class="text-slate-500 hover:text-slate-600 hover:underline" href="https://github.com/DominicSears/scripture_database_laravel">GitHub Project</a>
+                        <a class="text-slate-500 hover:text-slate-600 hover:underline" target="_blank" href="https://github.com/Apologenetics/Apologenetics">GitHub Project</a>
                         <!--<span> | </span>-->
                     </div>
                 </div>
