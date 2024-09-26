999e5b399292e4d2c4c7064543d9e1f773256e4a
Dominic Sears <dominicsears@hotmail.com>
2024-09-26 10:02:10 -0700

    Mobile hamburger menu and hide sidebar when on smaller viewports

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,28 +1,32 @@
-<div class="w-full flex flex-col items-center max-w-6xl relative">
+<div class="w-full flex flex-col items-center max-w-6xl">
     <!-- Search Bar -->
-    <div class="w-full flex flex-row items-center">
+    <div class="w-full flex flex-row items-center justify-center">
         <!-- Search Icon and Text Field -->
-        <div class="flex flex-row space-x-2 items-center bg-white rounded-lg w-11/12 py-2 pr-4 ring-1 ring-sky-200 shadow-lg">
+        <div
+            class="flex flex-row gap-2 items-center bg-white rounded-lg w-11/12 py-2 pr-4 ring-1 ring-sky-200 shadow-lg">
             <!-- Search Field -->
-            <input type="text"
-                wire:model.debounce.500ms="state.search"
-                wire:keyup.enter="redirectSearch"
+            <input type="text" wire:model.debounce.500ms="state.search" wire:keyup.enter="redirectSearch"
                 placeholder="Search for tags, doctrines, religions, denominations, etc..."
                 class="text-gray-600 placeholder-gray-400 text-md bg-white border-none flex-shrink-1 w-full focus:border-transparent focus:ring-0 focus:placeholder-transparent">
             <!-- Search Icon -->
             <button>
                 <svg class="h-6 w-6 text-sky-400" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M22 22L20 20" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path
+                        d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
+                        stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M22 22L20 20" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                        stroke-linejoin="round" />
                 </svg>
             </button>
         </div>
     </div>
-    @if (! empty($state['search']))
+    @if (!empty($state['search']))
         <!-- Results -->
-        <div x-data @click.away="$el.remove()" style="margin-top: 4.5rem;" class="origin-center absolute w-full rounded-md bg-white ring-1 ring-slate-400 divide-y divide-slate-500 focus:outline-none py-2 space-y-4 oerflow-y-auto max-h-64" tab-index="1">
+        <div x-data @click.away="$el.remove()" style="margin-top: 4.5rem;"
+            class="origin-center absolute w-full rounded-md bg-white ring-1 ring-slate-400 divide-y divide-slate-500 focus:outline-none py-2 space-y-4 oerflow-y-auto max-h-64"
+            tab-index="1">
             <div class="flex flex-col space-between space-y-2">
-                @if (! empty($searchResults))
+                @if (!empty($searchResults))
                     <!-- Search Items -->
                     <div class="flex flex-col space-y-2">
                         @foreach ($searchResults as $result)
@@ -35,10 +39,12 @@
                     </div>
                     <!-- Link for search page -->
                     <div class="flex flex-row items-center justify-end space-x-2 px-2 text-sky-500 hover:text-sky-700">
-                        <a class="text-sm font-semibold" href="{{ route('search.results', ['q' => urlencode($state['search'])]) }}">
+                        <a class="text-sm font-semibold"
+                            href="{{ route('search.results', ['q' => urlencode($state['search'])]) }}">
                             <span>See More</span>
                         </a>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
                         </svg>
                     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
 
 <head>
     <meta charset="utf-8">
@@ -17,10 +17,10 @@
     </style>
 </head>
 
-<body class="w-full h-screen">
-    <div class="flex flex-row w-full h-full">
+<body class="w-full h-screen bg-white dark:bg-gray-900 lg:overflow-y-auto">
+    <div class="flex flex-col lg:flex-row w-full h-full">
         <!-- Left -->
-        <div class="flex flex-col justify-between items-center w-3/12 bg-sky-100 p-8 ">
+        <div class="flex flex-col justify-between items-center w-full lg:w-3/12 bg-sky-100 dark:bg-sky-800 p-8">
             <!-- App Name -->
             <div class="flex flex-row justify-center items-center space-x-4 w-full">
                 <p class="text-2xl font-bold">{{ env('APP_NAME') }}</p>
@@ -38,7 +38,7 @@
                 </div>
             </div>
             <!-- Scripture & Slogan -->
-            <div class="w-full flex flex-col space-y-10 items-center justify-center">
+            <div class="w-full hidden lg:flex flex-col space-y-10 items-center justify-center">
                 <!-- Scripture -->
                 <div class="flex flex-row flex-wrap w-full justify-center">
                     <h1 class="text-4xl italic font-thin text-center">
@@ -118,18 +118,19 @@
                 </div>
                 <!-- Links -->
                 <div class="flex flex-row space-x-4 flex-wrap w-full justify-center">
-                    <a class="text-slate-500 hover:text-slate-600 hover:underline" target="_blank"
-                        href="https://github.com/Apologenetics/Apologenetics">GitHub Project</a>
+                    <a class="dark:text-slate-300 dark:hover:text-slate-400 text-slate-500 hover:text-slate-600 hover:underline"
+                        target="_blank" href="https://github.com/Apologenetics/Apologenetics">GitHub Project</a>
                     <!--<span> | </span>-->
                 </div>
             </div>
         </div>
         <!-- Right -->
-        <div class="flex flex-col w-9/12 h-screen overflow-y-auto">
+        <div class="flex flex-col w-full lg:w-9/12 h-screen overflow-y-auto">
             <div class="flex flex-col w-full h-full">
                 <!-- Sign up -->
                 <div class="flex flex-col w-full items-center h-full justify-center">
-                    <p class="text-lg text-slate-600 font-semibold mb-4">Sign Up for {{ config('app.name') }}</p>
+                    <p class="text-lg text-slate-600 dark:text-slate-200 font-semibold mb-4">Sign Up for
+                        {{ config('app.name') }}</p>
                     <a href="{{ route('register') }}"
                         class="px-4 py-2 ring-1 ring-slate-300 hover:bg-slate-100 text-slate-500 hover:text-slate-600 rounded-lg w-1/4 text-center">
                         <span>Sign Up</span>
@@ -139,19 +140,19 @@
                 <div class="flex flex-row w-full h-1/16 justify-between">
                     <!-- Line left -->
                     <div class="flex flex-col w-full h-full">
-                        <div class="h-full w-full border-b border-slate-200"></div>
+                        <div class="h-full w-full border-b border-slate-200 dark:border-slate-700"></div>
                         <div class="h-full w-full"></div>
                     </div>
-                    <p class="text-xl text-slate-400 font-semibold w-full text-center">Or Login</p>
+                    <p class="text-xl text-slate-400 dark:text-slate-300 font-semibold w-full text-center">Or Login</p>
                     <!-- Line right -->
                     <div class="flex flex-col w-full h-full">
-                        <div class="h-full w-full border-b border-slate-200"></div>
+                        <div class="h-full w-full border-b border-slate-200 dark:border-slate-700"></div>
                         <div class="h-full w-full"></div>
                     </div>
                 </div>
                 <!-- Login form -->
                 <form method="POST" action="{{ route('login') }}"
-                    class="w-full h-full flex flex-col space-y-4 items-center justify-center">
+                    class="w-full h-full flex flex-col space-y-4 items-center justify-center px-8 md:px-0">
                     @csrf
 
                     @if ($errors->any())
@@ -162,7 +163,7 @@
 
                     <!-- Username -->
                     <div @class([
-                        'flex flex-row w-1/4 py-2 px-4 space-x-4 rounded-xl ring-1 items-center text-slate-400',
+                        'flex flex-row w-full lg:w-1/4 max-w-xl py-2 px-4 space-x-4 rounded-xl ring-1 items-center text-slate-400',
                         'ring-red-500 placeholder-red-500' => $errors->any(),
                         'ring-slate-300 text-slate-400 placeholder-slate-400' => !$errors->any(),
                     ])>
@@ -172,11 +173,11 @@
                         </svg>
                         <input id="username" name="username" type="text" value="{{ old('username') }}" required
                             placeholder="username"
-                            class="w-full px-2 focus:placeholder-transparent focus:border-transparent focus:ring-0 outline-0 border-0" />
+                            class="w-full px-2 focus:placeholder-transparent focus:border-transparent focus:ring-0 outline-0 border-0 bg-transparent" />
                     </div>
                     <!-- Password -->
                     <div @class([
-                        'flex flex-row w-1/4 py-2 px-4 space-x-4 rounded-xl ring-1 items-center text-slate-400',
+                        'flex flex-row w-full lg:w-1/4 max-w-xl py-2 px-4 space-x-4 rounded-xl ring-1 items-center text-slate-400',
                         'ring-red-500 placeholder-red-500' => $errors->any(),
                         'ring-slate-300 text-slate-400 placeholder-slate-400' => !$errors->any(),
                     ])>
@@ -187,10 +188,11 @@
                         </svg>
                         <input id="password" name="password" type="password" value="{{ old('password') }}" required
                             placeholder="password"
-                            class="w-full px-2 focus:placeholder-transparent focus:border-transparent focus:ring-0 outline-0 border-0" />
+                            class="w-full px-2 focus:placeholder-transparent focus:border-transparent focus:ring-0 outline-0 border-0 bg-transparent" />
                     </div>
                     <!-- Forgot Password & Sign-in button -->
-                    <div class="flex flex-row items-center justify-between w-1/4 space-x-2">
+                    <div
+                        class="flex flex-row items-center justify-end lg:justify-between w-full max-w-xl lg:w-1/4 space-x-2">
                         <a class="text-sm text-slate-400 hover:text-slate-500 underline"
                             href="{{ route('password.request') }}">Forgot password?</a>
                         <button type="submit"
